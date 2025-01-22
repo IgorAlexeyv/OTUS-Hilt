a98c60b572adb93cc34089ec0efb3809d113c9df
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2025-01-22 16:06:52 +0100

    Providing engine externally

--- app/experiments/src/main/kotlin/otus/gbp/dependency/Car.kt
+++ app/experiments/src/main/kotlin/otus/gbp/dependency/Car.kt
@@ -1,14 +1,12 @@
 package otus.gbp.dependency
 
 fun main() {
-    val car = Car()
+    val car = Car(V4Engine(200))
     car.start()
     car.stop()
 }
 
-class Car {
-    private val engine = V4Engine(100)
-
+class Car(private val engine: Engine) {
     fun start() {
         engine.start()
     }
