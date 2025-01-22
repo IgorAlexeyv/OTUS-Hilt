cc7413b1c0744c3699fa37ef414682609b1c0f1d
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2025-01-22 16:09:24 +0100

    Providing engine externally

--- app/experiments/src/main/kotlin/otus/gbp/dependency/Car.kt
+++ app/experiments/src/main/kotlin/otus/gbp/dependency/Car.kt
@@ -1,9 +1,13 @@
 package otus.gbp.dependency
 
 fun main() {
-    val car = Car(V4Engine(200))
-    car.start()
-    car.stop()
+    val car1 = Car(V4Engine(200))
+    car1.start()
+    car1.stop()
+
+    val car2 = Car(V8Engine(400))
+    car2.start()
+    car2.stop()
 }
 
 class Car(private val engine: Engine) {
--- app/experiments/src/main/kotlin/otus/gbp/dependency/Engine.kt
+++ app/experiments/src/main/kotlin/otus/gbp/dependency/Engine.kt
@@ -7,7 +7,16 @@ interface Engine {
 
 class V4Engine(private val power: Int) : Engine {
     override fun start() {
-        println("Engine started with power $power")
+        println("V4 Engine started with power $power")
+    }
+    override fun stop() {
+        println("Engine stopped")
+    }
+}
+
+class V8Engine(private val power: Int) : Engine {
+    override fun start() {
+        println("V8 Engine started with power $power")
     }
     override fun stop() {
         println("Engine stopped")
